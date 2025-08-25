f4d9a374730e67aecbb8b110b4c9dd5be7608ea0
Nikhil Kalidasu <skdc13129@gmail.com>
2025-08-25 17:58:33 +0000

    Modernize PollNotifer.js

--- PollNotifer.js
+++ PollNotifer.js
@@ -1,45 +1,66 @@
+// ==UserScript==
+// @name         UT Instapoll Notifier
+// @description  Plays a beep sound when new polls are available on UT Instapoll
+// @version      1.0
+// @author       vudung45, nik875
+// @match        https://polls.la.utexas.edu/course/*
+// @grant        none
+// ==/UserScript==
+
 /*
 Instructions:
-	1. Login to your canvas, then redirect the webpage to UT Instapoll of the class.
-	2. Copy the snippet below
-	3. Then just paste it inside a Chrome or Firefox developer console to run
-	4. If you see the console print ("Looking for incomplete polls...") then the script is working
-
-This script should invoke a weird BEEP noise whenever there is a new poll published
+	1. Copy-paste this script as a custom script in Tampermonkey.
+ 	2. When a new poll is posted, a repeating beep will play for the duration of the poll.
+    3. You MUST interact with the webpage (clicking something, etc) to enable audio.
+		This is because of a browser security feature.
+	4. Turn down volume, it can be quite loud.
+ Tested to work on Firefox.
 */
-function PollNotifer(){
-	let a=new AudioContext()
-	function beep(vol, freq, duration){
-	  v=a.createOscillator()
-	  u=a.createGain()
-	  v.connect(u)
-	  v.frequency.value=freq
-	  v.type="square"
-	  u.connect(a.destination)
-	  u.gain.value=vol*0.01
-	  v.start(a.currentTime)
-	  v.stop(a.currentTime+duration*0.001)
-	}
-	let pollTableList = document.getElementsByClassName("table table-hover table-itemlist");
-	if(pollTableList.length > 0)
-	{
-
-		let pollTable = pollTableList[0]; 
-		let polls = pollTable.getElementsByTagName("tr");
-		for(var i = 1; i < polls.length; ++i)
-		{
-			let columns = polls[i].getElementsByTagName("td");
-			if(columns.length >= 3)
-			{
-				if(!columns[2].innerHTML.includes("Completed"))
-				{
-					beep(1000,100,100);
-				}
-			}
-		}
-		console.log("Looking for incomplete polls...");
-	}
-	setTimeout(function(){PollNotifer();},1000);
+function PollNotifier() {
+    let a = null; // Don't create AudioContext immediately
+    
+    function beep(vol, freq, duration) {
+        // Create AudioContext only when needed (after user gesture)
+        if (!a) {
+            try {
+                a = new AudioContext();
+                if (a.state === 'suspended') {
+                    a.resume();
+                }
+            } catch (e) {
+                console.log("Audio not available - click anywhere on the page first");
+                return;
+            }
+        }
+        
+        let v = a.createOscillator();
+        let u = a.createGain();
+        v.connect(u);
+        v.frequency.value = freq;
+        v.type = "square";
+        u.connect(a.destination);
+        u.gain.value = vol * 0.01;
+        v.start(a.currentTime);
+        v.stop(a.currentTime + duration * 0.001);
+    }
+    
+    let pollTableList = document.getElementsByClassName("table table-hover table-itemlist");
+    if (pollTableList.length > 0) {
+        let pollTable = pollTableList[0]; 
+        let polls = pollTable.getElementsByTagName("tr");
+        
+        for (let i = 1; i < polls.length; ++i) {
+            let columns = polls[i].getElementsByTagName("td");
+            if (columns.length >= 3) {
+                if (!columns[2].innerHTML.includes("Completed")) {
+                    beep(1000, 100, 100);
+                }
+            }
+        }
+        console.log("Looking for incomplete polls...");
+    }
+    
+    setTimeout(function() { PollNotifier(); }, 1000);
 }
 
-PollNotifer();
+PollNotifier();
